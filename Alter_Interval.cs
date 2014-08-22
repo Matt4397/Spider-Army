a4c84f8345b5e9b2e4af15deee2bbc851930bca1
Matthew Campbell <mcjc4397@gmail.com>
2014-08-22 14:58:40 -0700

    new stuff

---
+++ Alter_Interval.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alter_Interval : MonoBehaviour {
+
+	[HideInInspector]
+	public float interval;
+
+	// Use this for initialization
+	void Start () {
+		interval = GetComponent<AutoSpawner> ().timing.interval;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<AutoSpawner>().timing.interval = interval;
+	}
+}
---
+++ BackandForth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackandForth : MonoBehaviour {
+
+
+	public float speed = 10;
+	public float borderleft;
+	public float borderright;
+
+	// Use this for initialization
+	void Start () {
+			rigidbody2D.velocity = new Vector2 (speed, 0);
+			borderleft = transform.position.x + borderleft;
+			borderright = transform.position.x + borderright;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > borderright) { 
+			rigidbody2D.velocity = new Vector2 (-speed, 0);
+		}	
+		if (transform.position.x < borderleft) { 
+			rigidbody2D.velocity = new Vector2 (speed, 0);
+		
+		}
+	}
+}
