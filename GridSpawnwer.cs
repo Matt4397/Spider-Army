b88b21fd60838a11b1101c6cbb8f5d8974231bb3
Matthew Campbell <mcjc4397@gmail.com>
2014-08-29 14:40:36 -0700

    8/29/14

--- GridSpawnwer.cs
+++ GridSpawnwer.cs
@@ -4,21 +4,42 @@ using System.Collections;
 public class GridSpawnwer : MonoBehaviour {
 
 	public GameObject gridObject;
+	public int  IStart0;
+	public int  IWidth;
+	public int  IEnd1;
+	public int  JStart0;
+	public int  JHigth;
+	public int  JEnd1;
+	public int  BJStart1;
+	public int  BJHight;
+	public int  BIStart1;
+	public int  BIWidth;
+	public MethodButton _CreateGrid;
+
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log ("start");
+		CreateGrid ();
+	}
 
-		for (float i = 0; i < 4; i=i+1) {
-			for ( float j = 0; j < 5; j += 1){
-			GameObject spawned = stTools.Spawn (gridObject);
-			spawned.transform.position = new Vector2(transform.position.x + i,
-			                                    	 transform.position.y + j);
+	public void CreateGrid(){
+		Debug.Log ("create");
+		for (float i = IStart0; i < IWidth; i=i+ IEnd1) {
+			for ( float j = JStart0; j < JHigth; j += JEnd1){
+				if (j < BJStart1 || j >=BJHight || i < BIStart1 || i >= BIWidth) {
+					GameObject spawned = stTools.Spawn (gridObject);
+					spawned.transform.position = new Vector2(transform.position.x + i,
+					                                    	 transform.position.y + j);
+				}
 		}
 	}
 }
 	
 	// Update is called once per frame
 	void Update () {
+
+	//if (transform.position.x)
 	
 	}
 }
